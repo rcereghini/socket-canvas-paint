ff8ed600958ee317e76b2d9fbc26c024cf897443
Robert Cereghini <rcereghini@gmail.com>
2020-02-01 23:46:26 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("a user disconnected");
+  });
+
+  socket.on("testPing", function(msg) {
+    io.emit("pinged", msg);
+    console.log("pinggg!!!");
+  });
+
+  socket.on("userDrawing", function(canvasEvent) {
+    io.emit("userDrew", canvasEvent);
+    console.log("a user drew!", canvasEvent);
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
